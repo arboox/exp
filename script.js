b8f5af1c9203a3865720947bc365cc98be2b875e
josemanuelze <71357308+josemanuelze@users.noreply.github.com>
2023-05-12 15:46:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function getRandomItem(arr) {
     const item = arr[randomIndex];
     return item;
 }
-const array = ['https://exp.arboox.com/1/balustervase.html', 'https://exp.arboox.com/1/bettafish.html', 'https://exp.arboox.com/1/cameraargus.html', 'https://exp.arboox.com/1/moai.html', 'https://exp.arboox.com/1/oss.html'];
-const exp1 = getRandomItem(array);
+const arrayExp1 = ['https://exp.arboox.com/1/balustervase.html', 'https://exp.arboox.com/1/bettafish.html', 'https://exp.arboox.com/1/cameraargus.html', 'https://exp.arboox.com/1/moai.html', 'https://exp.arboox.com/1/oss.html'];
+const exp1 = getRandomItem(arrayExp1);
 document.getElementById('exp-1').src = exp1;
