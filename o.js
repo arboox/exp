a5db4f361b6c211817c234c0385716381c39aae7
josemanuelze <71357308+josemanuelze@users.noreply.github.com>
2023-05-12 21:15:54 -0600

    Create o.js

---
+++ o.js
@@ -0,0 +1,16 @@
+const experience1 = document.getElementById('exp-1');
+
+if (experience1.src = "https://exp.arboox.com/1/balustervase.html") {
+ document.getElementById('o-a-exp-1').src = "https://arboox.com/experience?name=Baluster+vase&src=https://cdn.glitch.global/a9c4e687-7c99-4065-88c1-02e69edfd394/baluster_vase_from_a_five-piece_garniture.glb?v=1681489367969&scene=https://images.unsplash.com/photo-1524079314971-dbe1837f967e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80&arp=&link=none&lt=&url=&cr=&ani=none&text=&ff=Plus+Jakarta+Sans&ta=center&fw=400&fs=none&fc=none";
+  document.getElementById('a-exp-1').src = "https://arboox.com/experience?name=Baluster+vase&src=https://cdn.glitch.global/a9c4e687-7c99-4065-88c1-02e69edfd394/baluster_vase_from_a_five-piece_garniture.glb?v=1681489367969&scene=https://images.unsplash.com/photo-1524079314971-dbe1837f967e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1935&q=80&arp=&link=none&lt=&url=&cr=&ani=none&text=&ff=Plus+Jakarta+Sans&ta=center&fw=400&fs=none&fc=none";
+        document.querySelector("#pp-t-1").addEventListener("click", function () {
+  document.querySelector("#discover-t").style.color = "#ababab";
+  document.querySelector("#pp-tb-i").style.display = "none";
+  document.querySelector("#pp-tb-color").style.display = "block";
+  document.querySelector("#pp-overlay").style.display = "block";
+  document.querySelector("#pp-ins").style.display = "block";
+        document.querySelector("#pp-frame").src = "@/art21";
+        document.querySelector("#o-5").className = "m immerse";
+        document.querySelector("#overlay-o").style.display = "none";
+});
+}
