f5f4901584afde3c1cbcd28b40898b3c10141090
josemanuelze <71357308+josemanuelze@users.noreply.github.com>
2023-05-13 09:07:36 -0600

    Create script-zh.js

---
+++ script-zh.js
@@ -0,0 +1,58 @@
+function getRandomItemExp1(arr) {
+    const randomIndexExp1 = Math.floor(Math.random() * arr.length);
+    const itemExp1 = arr[randomIndexExp1];
+    return itemExp1;
+}
+const arrayExp1 = ['https://exp.arboox.com/1/balustervase.html?t=打开', 'https://exp.arboox.com/1/bettafish.html?t=打开', 'https://exp.arboox.com/1/cameraargus.html?t=打开', 'https://exp.arboox.com/1/moai.html?t=打开', 'https://exp.arboox.com/1/oss.html?t=打开'];
+const exp1 = getRandomItemExp1(arrayExp1);
+document.getElementById('exp-1').src = exp1;
+
+
+function getRandomItemExp2(arr) {
+    const randomIndexExp2 = Math.floor(Math.random() * arr.length);
+    const itemExp2 = arr[randomIndexExp2];
+    return itemExp2;
+}
+const arrayExp2 = ['https://exp.arboox.com/2/intercitytrain.html?t=打开', 'https://exp.arboox.com/2/kangaroo.html?t=打开', 'https://exp.arboox.com/2/painting.html?t=打开', 'https://exp.arboox.com/2/ponycar.html?t=打开', 'https://exp.arboox.com/2/violin.html?t=打开'];
+const exp2 = getRandomItemExp2(arrayExp2);
+document.getElementById('exp-2').src = exp2;
+
+
+function getRandomItemExp3(arr) {
+    const randomIndexExp3 = Math.floor(Math.random() * arr.length);
+    const itemExp3 = arr[randomIndexExp3];
+    return itemExp3;
+}
+const arrayExp3 = ['https://exp.arboox.com/3/cruiseship.html?t=打开', 'https://exp.arboox.com/3/macaw.html?t=打开', 'https://exp.arboox.com/3/nefertiti.html?t=打开', 'https://exp.arboox.com/3/prairiedog.html?t=打开', 'https://exp.arboox.com/3/woodenboat.html?t=打开'];
+const exp3 = getRandomItemExp3(arrayExp3);
+document.getElementById('exp-3').src = exp3;
+
+
+function getRandomItemExp4(arr) {
+    const randomIndexExp4 = Math.floor(Math.random() * arr.length);
+    const itemExp4 = arr[randomIndexExp4];
+    return itemExp4;
+}
+const arrayExp4 = ['https://exp.arboox.com/4/filmcamera.html?t=打开', 'https://exp.arboox.com/4/hall.html?t=打开', 'https://exp.arboox.com/4/hummingbird.html?t=打开', 'https://exp.arboox.com/4/perseverance.html?t=打开', 'https://exp.arboox.com/4/shark.html?t=打开'];
+const exp4 = getRandomItemExp4(arrayExp4);
+document.getElementById('exp-4').src = exp4;
+
+
+function getRandomItemExp5(arr) {
+    const randomIndexExp5 = Math.floor(Math.random() * arr.length);
+    const itemExp5 = arr[randomIndexExp5];
+    return itemExp5;
+}
+const arrayExp5 = ['https://exp.arboox.com/5/alpaca.html?t=打开', 'https://exp.arboox.com/5/cnsupplystore.html?t=打开', 'https://exp.arboox.com/5/facttomato.html?t=打开', 'https://exp.arboox.com/5/monstera.html?t=打开', 'https://exp.arboox.com/5/seahorse.html?t=打开'];
+const exp5 = getRandomItemExp5(arrayExp5);
+document.getElementById('exp-5').src = exp5;
+
+
+function getRandomItemExp6(arr) {
+    const randomIndexExp6 = Math.floor(Math.random() * arr.length);
+    const itemExp6 = arr[randomIndexExp6];
+    return itemExp6;
+}
+const arrayExp6 = ['https://exp.arboox.com/6/babyelephant.html?t=打开', 'https://exp.arboox.com/6/bonsai.html?t=打开', 'https://exp.arboox.com/6/ineedbeach.html?t=打开', 'https://exp.arboox.com/6/plane.html?t=打开', 'https://exp.arboox.com/6/underground.html?t=打开'];
+const exp6 = getRandomItemExp6(arrayExp6);
+document.getElementById('exp-6').src = exp6;
