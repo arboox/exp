37bdd29e6b3a8668769d668d075fe97d1e9a7ef3
josemanuelze <71357308+josemanuelze@users.noreply.github.com>
2023-05-13 08:07:52 -0600

    Delete script.js

--- 2/script.js
+++
@@ -1,8 +0,0 @@
-function getRandomItemExp2(arr) {
-    const randomIndexExp2 = Math.floor(Math.random() * arr.length);
-    const itemExp2 = arr[randomIndexExp2];
-    return itemExp2;
-}
-const arrayExp2 = ['https://exp.arboox.com/2/intercitytrain.html', 'https://exp.arboox.com/2/kangaroo.html', 'https://exp.arboox.com/2/painting.html', 'https://exp.arboox.com/2/ponycar.html', 'https://exp.arboox.com/2/violin.html'];
-const exp2 = getRandomItemExp2(arrayExp2);
-document.getElementById('exp-2').src = exp2;
